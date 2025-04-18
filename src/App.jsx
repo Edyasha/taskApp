130bd587236406c38d7d93c387eebafeabaf3171
Edison <edyasha00@gmail.com>
2025-04-18 16:14:41 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+    <div className=''>
+      <p className="text-3xl font-bold underline">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  )
+}
+
+export default App
